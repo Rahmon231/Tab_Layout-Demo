3e20f841d985cd667531d0b703d97a6b3e5f8e3b
Akanbi Rahmon <23046879+Rahmon231@users.noreply.github.com>
2022-07-10 00:00:01 +0100

    initial commit

---
+++ app/src/main/java/com/lemzeeyyy/tablayoutdemo/MainActivity.java
@@ -0,0 +1,41 @@
+package com.lemzeeyyy.tablayoutdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+    private TabLayout tabLayout;
+    private ViewPager2 viewPager2;
+    private ViewPagerAdapter viewPagerAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tabLayout = findViewById(R.id.tab_layout);
+        viewPager2 = findViewById(R.id.view_pager);
+        viewPagerAdapter = new ViewPagerAdapter(this);
+        viewPager2.setAdapter(viewPagerAdapter);
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager2.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/lemzeeyyy/tablayoutdemo/ViewPagerAdapter.java
@@ -0,0 +1,36 @@
+package com.lemzeeyyy.tablayoutdemo;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import com.lemzeeyyy.tablayoutdemo.fragments.HomeFragment;
+import com.lemzeeyyy.tablayoutdemo.fragments.NotificationFragment;
+import com.lemzeeyyy.tablayoutdemo.fragments.SettingsFragment;
+
+public class ViewPagerAdapter extends FragmentStateAdapter {
+    public ViewPagerAdapter(@NonNull FragmentActivity fragmentActivity) {
+        super(fragmentActivity);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        switch (position){
+            case 0:
+                return new HomeFragment();
+            case 1:
+                return new NotificationFragment();
+            case 2:
+                return new SettingsFragment();
+            default:
+                return new HomeFragment();
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return 3;
+    }
+}
